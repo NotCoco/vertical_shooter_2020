f24f329529a2c3abc5f1b4971caabc77dcdf1eac
Kerim <k1889476@kcl.ac.uk>
2020-07-08 18:55:15 +0100

    Add target finding functionality
    The cannon can now lock onto the nearest target on the scene

---
+++ 2D_VS/Assets/Scripts/AimController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimController : MonoBehaviour
+{
+    public static ArrayList objects;
+    public GameObject cannon;
+    void Awake()
+    {
+        objects = new ArrayList();
+        //T[] asList = GameObject.FindObjectsOfType<Enemy>();
+        //objects = new ArrayList(asList);
+    }
+
+    public static void AddToList(Enemy enemy)
+    {
+        objects.Add(enemy);
+    }
+
+    public static void RemoveFromList(Enemy enemy)
+    {
+        objects.Remove(enemy);
+    }
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Enemy closest = FindClosestEnemy();
+            Debug.DrawLine(cannon.transform.position, closest.transform.position, Color.red, 10.00f);
+            //Debug.Log(closest.transform.position.x + ", " + closest.transform.position.y);
+        }
+
+    }
+
+
+    Enemy FindClosestEnemy()
+    {
+        float distanceClosest = Mathf.Infinity;
+        Enemy closestEnemy = null;
+        foreach (Enemy enemy in objects)
+        {
+            float distanceToEnemy = (enemy.transform.position - cannon.transform.position).sqrMagnitude;
+            if (distanceToEnemy < distanceClosest)
+            {
+                closestEnemy = enemy;
+                distanceClosest = distanceToEnemy;
+            }
+        }
+        return closestEnemy;
+    }
+}
---
+++ 2D_VS/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AimController.AddToList(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Destroy()
+    {
+        AimController.RemoveFromList(this);
+        Destroy(gameObject);
+    }
+}
