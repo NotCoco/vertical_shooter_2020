5e42179aa3f69df15d59b83176df9fca4887533f
Kerim <k1889476@kcl.ac.uk>
2020-07-09 23:47:20 +0100

    Introduce character movement via touchscreen

--- 2D_VS/Assets/Scripts/AimController.cs
+++ 2D_VS/Assets/Scripts/AimController.cs
@@ -6,9 +6,15 @@ public class AimController : MonoBehaviour
 {
     public static ArrayList objects;
     public GameObject cannon;
+    private Transform aimTransform;
+
+    float retarget_time = 1.00f;
     void Awake()
     {
+
         objects = new ArrayList();
+        aimTransform = transform.Find("Cannon_Head");
+        //aimTransform = transform.Find("Aim");
         //T[] asList = GameObject.FindObjectsOfType<Enemy>();
         //objects = new ArrayList(asList);
     }
@@ -29,13 +35,17 @@ public class AimController : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (retarget_time < 0)
         {
             Enemy closest = FindClosestEnemy();
             Debug.DrawLine(cannon.transform.position, closest.transform.position, Color.red, 10.00f);
-            //Debug.Log(closest.transform.position.x + ", " + closest.transform.position.y);
+            Vector3 enemyPosition = closest.transform.position;
+            Vector3 aimDirection = (enemyPosition - cannon.transform.position).normalized;
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+            aimTransform.eulerAngles = new Vector3(0, 0, angle - 90);
+            retarget_time = 1.00f;
         }
-
+        retarget_time -= Time.deltaTime;
     }
 
 
---
+++ 2D_VS/Assets/Scripts/CannonController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    private Vector2 startJPos; // the starting position of the joystick
+    private Vector2 endJPos; // the ending position
+    private bool touchStart = false; // joystick movement bool
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    private Rigidbody2D rigbody;
+
+    public float speed = 0.1f;
+    void Start()
+    {
+        rigbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { // when the screen is initially tapped
+            touchStart = true;
+            //startJPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.00f));
+            startJPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            //endJPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.00f));
+            endJPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        else
+        {
+            touchStart = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = endJPos - startJPos;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+            Vector2 t = transform.position;
+            rigbody.MovePosition(t + direction * 0.08f);
+
+        }
+    }
+}
